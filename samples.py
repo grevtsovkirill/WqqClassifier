dc519075f29c85d47674cc3e050914f10c34834a
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-28 11:22:13 +0200

    information regarding sampkes

---
+++ samples.py
@@ -0,0 +1,19 @@
+###
+BASE       = '../../Files/processed/'
+
+####
+samples = {    
+    'ttW' : {
+        'filename' : 'skimReco_413008_xs.csv',   
+        'color'    : 'red',   
+    },
+    'ttZmumu' : {
+        'filename' : 'skimReco_410219_xs.csv',   
+        'group'   : 'ttZ',   
+        'color'    : 'blue',   
+    },    
+    # 'ttbar' : {
+    #     'filename' : 'mc16_13TeV.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad_r9364_p3980.roott',   
+    #     'color'    : 'green',   
+    # }
+}
