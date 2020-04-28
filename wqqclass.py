d87e899d50905bc4cfe2bd06e38b92036c2d51f1
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-28 11:36:23 +0200

    initialise the code to load data classes. resolves #1

--- wqqclass.py
+++ wqqclass.py
@@ -3,7 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
+from samples import *
+
+def data_load(in_list, do_clean=False):
+    df = {}
+    if do_clean:
+        var_list=sel_vars()
+
+    for s in samples:
+        print(s,'  ',samples[s]['filename'])
+        df[s] = pd.read_csv(BASE+samples[s]['filename'])
+        if do_clean:
+            df[s] = df[s][var_list]
+    return df
+
 def main():
     print("load data")
+    dfs=data_load(samples)
+     
 if __name__ == "__main__":
     main() 
