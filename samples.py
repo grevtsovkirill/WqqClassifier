bae80c9d56f439226b0cd80f187ab928df08e328
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-28 14:37:25 +0200

    add ttbar

--- samples.py
+++ samples.py
@@ -12,8 +12,8 @@ samples = {
         'group'   : 'ttZ',   
         'color'    : 'blue',   
     },    
-    # 'ttbar' : {
-    #     'filename' : 'mc16_13TeV.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad_r9364_p3980.roott',   
-    #     'color'    : 'green',   
-    # }
+    'ttbar' : {
+        'filename' : 'skimReco_410470_xs.csv',   
+        'color'    : 'green',   
+    }
 }
