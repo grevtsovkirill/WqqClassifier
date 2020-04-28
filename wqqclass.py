4a6e20d7b91dc0506797e07abaedaac8f9967c0d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-28 16:29:50 +0200

    intial verion of the model (require optimization). resolves #4

--- wqqclass.py
+++ wqqclass.py
@@ -6,7 +6,9 @@ import json
 from samples import *
 
 
-from sklearn.model_selection import train_test_split
+seed=8
+np.random.seed(seed)
+
 
 
 import argparse
@@ -20,6 +22,13 @@ args = parser.parse_args()
 process_type = vars(args)["type"]
 sample_list = vars(args)["samples"]
 doclean = vars(args)["clean"]
+
+if process_type =='train':
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    from keras.layers import Layer, Input, Dense, Dropout
+    from keras.models import Sequential, load_model
+
 
 scale_to_GeV=0.001
 binning = {"DRll01": np.linspace(-2, 6, 24),
@@ -92,10 +101,50 @@ def plot_var(df_bkg,lab_list,var,do_stack=True,GeV=1):
 
 def pred_ds(dfs,test_samp_size=0.33):    
     X = np.concatenate((dfs['ttW'],dfs['ttbar']))
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
     y = np.concatenate((np.ones(dfs['ttW'].shape[0]),np.zeros(dfs['ttbar'].shape[0]))) # class lables                                                                       
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_samp_size)
     return X_train, X_test, y_train, y_test
 
+def create_model(my_learning_rate):
+    dense_dim=len(sel_vars())
+    model = Sequential()
+    model.add(Dense(dense_dim, input_dim=dense_dim, activation='relu'))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dense(35, activation='relu'))
+    model.add(Dropout(rate=0.1, noise_shape=None, seed=None))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1, activation='sigmoid',name='classifier_output'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+def train_model(model, train_features, train_label, epochs,
+                batch_size=None, validation_split=0.1):
+    history = model.fit(x=train_features, y=train_label, batch_size=batch_size,
+                      epochs=epochs, shuffle=True, 
+                      validation_split=validation_split)
+    epochs = history.epoch
+    hist = pd.DataFrame(history.history)
+
+    return epochs, hist
+
+def plot_curve(epochs, hist, list_of_metrics,save=True):
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Value")
+    
+    for m in list_of_metrics:
+        x = hist[m]
+        plt.plot(epochs[1:], x[1:], label=m)
+    
+    plt.ylabel('cross-entropy loss',fontsize=14)
+    plt.xlabel('epochs',fontsize=14)
+    plt.legend()
+    if save:
+        plt.savefig("Plots/training/loss_NNw.png", transparent=True)
+    else:
+        plt.show()
 
 def main():
     print("load data")
@@ -110,7 +159,21 @@ def main():
             print("prepare for training, ")
             X_train, X_test, y_train, y_test = pred_ds(dfs)
             
-        
-     
+            learning_rate = 0.003
+            epochs = 500
+            batch_size = 16000
+            validation_split = 0.2
+
+            mymodel = create_model(learning_rate)        
+            epochs, hist = train_model(mymodel, X_train, y_train, 
+                           epochs, batch_size, validation_split)
+
+            print("\n Evaluate the new model against the test set:")
+            print(mymodel.evaluate(X_test, y_test, batch_size=batch_size))
+            
+            list_of_metrics_to_plot = ['loss','val_loss']
+            plot_curve(epochs, hist, list_of_metrics_to_plot)
+            
+            
 if __name__ == "__main__":
     main() 
