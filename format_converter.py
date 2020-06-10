d102b5f59407acab09472358734671d7ad8009f3
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-06-10 14:45:08 +0200

    update convertor to account for comp. resolves #11

--- format_converter.py
+++ format_converter.py
@@ -1,10 +1,14 @@
 import os
 
-samples = ['413008','304014','345874','345875','363356','363357','363358','363359','364285','410081','410156','410157','410218','410219','410220']
-#samples = ['410470']     
+#samples = ['413008','304014','345874','345875','363356','363357','363358','363359','364285','410081','410156','410157','410218','410219','410220']
+samples = ['410470','700000']     
+comp = ['a','d','e']
 #option = ""
+ver = "v9"
+
 for i in samples:
-    tick = "'"
-    command = 'python convertor.py -i ../Files/ -f skimReco_'+i+'_xs -t outTree'
-    print(command)
-    os.system(command)
+    for j in comp:
+        tick = "'"
+        command = 'python convertor.py -i ../Files/'+ver+'/ -f skimReco_'+i+'_'+j+' -t outTree -o ../Files/processed'
+        print(command)
+        os.system(command)
